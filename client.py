3568a15f1c4389847bbc32ab7a859a436fdddff8
neoni <blue.snow013@163.com>
2021-07-27 23:04:39 -0700

    upgrade to 1.7.0 apk

--- client.py
+++ client.py
@@ -21,9 +21,9 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-APP_VER = "1.6.0"
-RES_VER = "10002000:TWuPn1NSQA/8"
-UMA_PUBKEY = "6b20e2ab6c311330f761d737ce3f3025750850665eea58b6372f8d2f57501eb36d5c30ba27c938f5bca9d1d99064981951dbca43"
+APP_VER = "1.7.0"
+RES_VER = "10002150:TaoV5lq3QuUx"
+UMA_PUBKEY = "6b20e2ab6c311330f761d737ce3f3025750850665eea58b6372f8d2f57501eb341fe3a08187b9a37b9ab1ca33c43b6c3202bdd0d"
 
 USER_AGENT = "Mozilla/5.0 (Linux; Android 10; SM-A102U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.72 Mobile Safari/537.36"
 
